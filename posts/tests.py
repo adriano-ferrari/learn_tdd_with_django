c77212e3ab303b18a5f1953f806b6283ff1dcbad
adriano-ferrari <irarrefonairda@gmail.com>
2024-03-01 09:19:24 -0300

    Building a list View | Learn Test Driven Development With Django #3

--- posts/tests.py
+++ posts/tests.py
@@ -11,26 +11,28 @@ class PostModelTest(TestCase):
         self.assertEqual(posts, 0)
 
     def test_string_rep_of_objects(self):
-        post = Post.objects.create(
-            title='Test Post',
-            body='Test Body'
-        )
+        post = Post.objects.create(title="Test Post", body="Test Body")
         self.assertEqual(str(post), post.title)
 
 
 class HomepageTest(TestCase):
     def setUp(self):
-        Post.objects.create(
-            title='sample post 1',
-            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin tristique semper. Nulla porttitor lorem leo, ut ornare leo lobortis ut. Phasellus ut finibus purus. In elementum a nibh nec cursus. Donec et nisl non mi mattis auctor. Praesent sit amet gravida nisl, vitae pulvinar diam. Duis vel pulvinar dui. Aenean a purus metus. Morbi et volutpat nulla, at congue ligula. Fusce tristique turpis vitae tincidunt ullamcorper. Sed vestibulum eros quis justo faucibus malesuada. Phasellus a eros venenatis, eleifend ex in, efficitur nunc. Donec quis dapibus dui.'
+        post1 = Post.objects.create(
+            title="sample post 1",
+            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin tristique semper. Nulla porttitor lorem leo, ut ornare leo lobortis ut. Phasellus ut finibus purus. In elementum a nibh nec cursus. Donec et nisl non mi mattis auctor. Praesent sit amet gravida nisl, vitae pulvinar diam. Duis vel pulvinar dui. Aenean a purus metus. Morbi et volutpat nulla, at congue ligula. Fusce tristique turpis vitae tincidunt ullamcorper. Sed vestibulum eros quis justo faucibus malesuada. Phasellus a eros venenatis, eleifend ex in, efficitur nunc. Donec quis dapibus dui.",
         )
 
-        Post.objects.create(
-            title='sample post 2',
-            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin tristique semper. Nulla porttitor lorem leo, ut ornare leo lobortis ut. Phasellus ut finibus purus. In elementum a nibh nec cursus. Donec et nisl non mi mattis auctor. Praesent sit amet gravida nisl, vitae pulvinar diam. Duis vel pulvinar dui. Aenean a purus metus. Morbi et volutpat nulla, at congue ligula. Fusce tristique turpis vitae tincidunt ullamcorper. Sed vestibulum eros quis justo faucibus malesuada. Phasellus a eros venenatis, eleifend ex in, efficitur nunc. Donec quis dapibus dui.'
+        post2 = Post.objects.create(
+            title="sample post 2",
+            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin tristique semper. Nulla porttitor lorem leo, ut ornare leo lobortis ut. Phasellus ut finibus purus. In elementum a nibh nec cursus. Donec et nisl non mi mattis auctor. Praesent sit amet gravida nisl, vitae pulvinar diam. Duis vel pulvinar dui. Aenean a purus metus. Morbi et volutpat nulla, at congue ligula. Fusce tristique turpis vitae tincidunt ullamcorper. Sed vestibulum eros quis justo faucibus malesuada. Phasellus a eros venenatis, eleifend ex in, efficitur nunc. Donec quis dapibus dui.",
         )
 
     def test_homepage_returns_correct_response(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'posts/index.html')
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "posts/index.html")
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_homepage_returns_post_list(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'sample post 1')
+        self.assertContains(response, 'sample post 2')
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from posts.models import Post
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+    return render(request, "posts/index.html", {'posts':posts})
